bc0687060578e457d3d1d57a9e11f588fac9715f
jogapps <jogappsdeveloper@gmail.com>
2021-04-30 12:04:52 +0100

    double linked list completed

--- double_linked_list_data_str.js
+++ double_linked_list_data_str.js
@@ -26,4 +26,110 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if(!this.head)  return undefined;
+        let poppedNode = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
+    shift() {
+        if(this.length === 0) return undefined;
+        let oldHead = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    unshift(val) {
+        let newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    } 
+
+    get(index) {
+        if(index < 0 || index > this.length) return null;
+        let count, current;
+        if(index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while(count != index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count != index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    } 
+
+    set(index, value) {
+        let foundNode = this.get(index);
+        if(foundNode != null) {
+            foundNode.val = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, value) {
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(value);
+        if(index === this.length) return !!this.push(value);
+        let newNode = new Node(value);
+        let beforeNode = this.get(index - 1);
+        let afterNode = beforeNode.next;
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return false;
+        if(index === 0) return !!this.shift();
+        if(index === this.length - 1) return !!this.pop();
+        let removedNode = this.get(index);
+        let beforeNode = removedNode.prev;
+        let afterNode = removedNode.next;
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+//         removedNode.prev.next = removedNode.next;
+//         removedNode.next = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+        this.length--;
+        return removedNode;
+    }
 }
+
+let first = new DoublyLinkedList();
+first.push(1);
+first.push(2);
+first.push(3);
+first.push(4);
